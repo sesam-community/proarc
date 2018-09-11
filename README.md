5c436312b20a064603f9c43bd265eecbb34b7cb9
Ashkan Vahidishams <ashkan.vahidishams@bouvet.no>
2018-09-11 17:14:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# SOAP-sink built with Zeep
+
+A small microservice to post data to a SOAP endpoint.
+All entities must have a "_soapheaders" attribute.
+
+[![Build Status](https://travis-ci.org/sesam-community/proarc.svg?branch=master)](https://travis-ci.org/sesam-community/proarc)
+
+##### Example entity
+```
+{
+  "_ts": 1486128503194780,
+  "_previous": null,
+  "_hash": "50d93095f6fb68e7517ea89e62c60af8",
+  "_id": "29932",
+  "_deleted": false,
+  "_updated": 8,
+  "_soapheaders": {
+    "header": {
+      [...]
+    }
+  },
+  "Medarbeider": {
+    [...]
+  }
+}
+```
+##### Example configuration:
+
+```
+{
+  "_id": "soap-service",
+  "type": "system:microservice",
+  "docker": {
+    "environment": {
+      "method": "createMedarbeiderEntity",
+      "url": "http://localhost:8088/MedarbeiderAdapterV1_1?WSDL",
+      # optional values below
+      "authentication": "basic",
+      "username": "theUsername",
+      "password": "thePassword",
+      "timeout": 30,
+      "cipher": ":ECDHE-RSA-AES128-SHA",
+      "transit_decode": "false"
+    },
+    "image": "sesambuild/soap-zeep-sink:latest",
+    "port": 5001
+  }
+}
+```
+##### Example configuration JWT:
+
+```
+{
+  "_id": "soap-service",
+  "type": "system:microservice",
+  "docker": {
+    "environment": {
+      "method": "createMedarbeiderEntity",
+      "url": "http://localhost:8088/MedarbeiderAdapterV1_1?WSDL",
+      "jwt_secret": "somesecret",
+      "jwt_expiry": "400",
+      "jwt_issuer": "theIssuer",
+      "timeout": 30,
+      "cipher": ":ECDHE-RSA-AES128-SHA",
+      "transit_decode": "true"
+    },
+    "image": "sesambuild/soap-zeep-sink:latest",
+    "port": 5001
+  }
+}
+```
---
+++ service/proarc.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+
+import requests
+from flask import Flask, request, Response
+from requests.auth import HTTPBasicAuth  # or HTTPDigestAuth, or OAuth1, etc.
+from zeep import Client
+from zeep.transports import Transport
+import logger
+import typetransformer
+import os
+
+rootlogger=logger.Logger()
+
+app = Flask(__name__)
+
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += os.environ.get('cipher', ':ECDHE-RSA-AES128-SHA')
+timeout=int(os.environ.get('timeout', '30'))
+url=os.environ.get('url')
+
+auth = os.environ.get('authentication', "")
+if auth.lower() == "basic":
+    rootlogger.info("Using authentication")
+    transport = Transport(http_auth=HTTPBasicAuth(os.environ.get('username'), os.environ.get('password')), timeout=timeout)
+else:
+    rootlogger.info("Skipping authentication")
+    transport = Transport(timeout=timeout)
+
+client = Client(url, transport=transport)
+##Receiving soap-object
+@app.route('/toproarc', methods=['POST'])
+def push():
+
+    entity = request.get_json()
+    file_url = entity[os.environ.get('file_url')]
+    file_name = entity[os.environ.get('file_name')]
+
+    download_file(file_url, file_name)
+    #removing entities here since they are not part of the soap call and will make the soap call fail
+    del entity[file_url]
+    del entity[file_name]
+
+    #Continuing on the soap call
+    if isinstance(entity, list):
+        return Response("Multiple entities is not supported",status=400, mimetype='text/plain')
+
+    if os.environ.get('transit_decode', 'false').lower() == "true":
+        rootlogger.info("transit_decode is set to True.")
+        entity = typetransformer.transit_decode(entity)
+
+    rootlogger.info("Finished creating request: " + str(entity))
+
+    response=do_soap(entity,client)
+    rootlogger.info("SOAPResponse : \n" + str(response) + "\n----End-Response----")
+    return Response("Thanks", mimetype='text/plain')
+
+
+def download_file(url, local_filename):
+
+    # NOTE the stream=True parameter
+    r = requests.get(url, stream=True)
+    with open("/fileshare/"+local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                #f.flush() commented by recommendation from J.F.Sebastian
+    return local_filename
+
+def do_soap(entity, client):
+
+    headers = entity['_soapheaders']
+    filtered_entity = {i:entity[i] for i in entity if not i.startswith('_') }
+    filtered_entity['_soapheaders']=headers
+
+    response = getattr(client.service, os.environ.get('method'))(**filtered_entity)
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('port',5000))
