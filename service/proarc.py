ba38630cf54226d6a8adb52080b44817a6c317b5
Timur Samkharadze <timur.samkharadze@sysco.no>
2019-08-06 13:38:14 +0200

    Service refactoring with respect to breaking changes in dependencies

--- service/proarc.py
+++ service/proarc.py
@@ -1,134 +1,181 @@
 #! /usr/bin/env python
-from logging import root
-
-import requests
+"""
+Pro Arc fetch and push service
+"""
+import os
+import logging
 from flask import Flask, request, Response
-from requests.auth import HTTPBasicAuth  # or HTTPDigestAuth, or OAuth1, etc.
+import urllib3
+import requests
+from requests import Session
+from requests.auth import HTTPBasicAuth
 from zeep import Client
 from zeep.transports import Transport
 import logger
-import logging
 import typetransformer
-import os
 
-rootlogger=logger.Logger()
+LOG = logger.logger()
 
-app = Flask(__name__)
+APP = Flask(__name__)
 PORT = int(os.environ.get('PORT', '5000'))
 
-requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += os.environ.get('cipher', ':ECDHE-RSA-AES128-SHA')
-timeout=int(os.environ.get('timeout', '30'))
-url=os.environ.get('url')
+CIPHER_TO_ADD = os.environ.get('cipher', ':ECDHE-RSA-AES128-SHA')
+urllib3.util.ssl_.DEFAULT_CIPHERS += CIPHER_TO_ADD
 
-auth = os.environ.get('authentication', "")
-if auth.lower() == "basic":
-    rootlogger.info("Using authentication")
-    transport = Transport(http_auth=HTTPBasicAuth(os.environ.get('username'), os.environ.get('password')), timeout=timeout)
-else:
-    rootlogger.info("Skipping authentication")
-    transport = Transport(timeout=timeout)
+TIMEOUT = int(os.environ.get('timeout', '30'))
+URL = os.environ.get('url', '')
+AUTH = os.environ.get('authentication', "")
 
-client = Client(url, transport=transport)
+FILE_URL_KEY = os.environ.get('file_url')
+FILE_NAME_KEY = os.environ.get('file_name')
 
 
-##Receiving soap-object
-@app.route('/toproarc/<path:path>', methods=['POST'])
+def get_soap_client():
+    """
+    function to build and return SOAP client
+    :return: zeep soap client object
+    """
+    if AUTH.lower() == "basic":
+        username = os.environ.get('username')
+        password = os.environ.get('password')
+        LOG.info("Using authentication")
+        session = Session()
+        session.auth = HTTPBasicAuth(username, password)
+        transport = Transport(session=session, timeout=TIMEOUT)
+    else:
+        LOG.info("Skipping authentication")
+        transport = Transport(timeout=TIMEOUT)
+
+    return Client(URL, transport=transport)
+
+
+SOAP_CLIENT = get_soap_client()
+
+
+@APP.route('/toproarc/<path:path>', methods=['POST'])
 def toproarc(path):
-
+    """
+    Funciton to dosnload file based on data from provided entity and store it in ProArc
+    :param path: SOAP method to call see https://proarctest-akersolutions.msappproxy.net/
+    FileManager section
+    :return: 200 Response if everything is ok
+    """
     if path is None:
-        return Response("Missing path/method to WS", status=500, mimetype='text/plain')
+        return Response("Missing path/method to WS", status=400, mimetype='text/plain')
 
     entity = request.get_json()
 
     if isinstance(entity, list):
-        return Response("Multiple entities is not supported",status=400, mimetype='text/plain')
+        return Response("Multiple entities is not supported", status=400, mimetype='text/plain')
 
     download_file(entity[os.environ.get('file_url')], entity[os.environ.get('file_name')])
 
-    #removing entities here since they are not part of the soap call and will make the soap call fail
-    del entity[os.environ.get('file_url')]
-    del entity[os.environ.get('file_name')]
+    # removing entities here since they are not part of the soap call
+    # and will make the soap call fail
+    del entity[FILE_URL_KEY]
+    del entity[FILE_NAME_KEY]
 
-    #Continuing on the soap call
+    # Continuing on the soap call
     if os.environ.get('transit_decode', 'false').lower() == "true":
-        rootlogger.info("transit_decode is set to True.")
+        LOG.info("transit_decode is set to True.")
         entity = typetransformer.transit_decode(entity)
 
-    rootlogger.info("Finished creating request: " + str(entity))
+    LOG.info(f'Finished creating request: {str(entity)}')
 
-    response=do_soap(entity,client, path)
-    rootlogger.info("SOAPResponse : \n" + str(response) + "\n----End-Response----")
+    response = do_soap(entity, SOAP_CLIENT, path)
+    LOG.info(f"SOAPResponse : \n{str(response)}\n----End-Response----")
     return Response("Thanks", mimetype='text/plain')
 
 
-##Receiving soap-object
-@app.route('/fromproarc/<path:path>', methods=['GET'])
+@APP.route('/fromproarc/<path:path>', methods=['GET'])
 def fromproarc(path):
-
+    """
+    Function to receive a file from Pro Arc
+    :param path:
+    :return:
+    """
     file_id = request.args.get('file_id')
     filename = request.args.get('filename')
 
     entity = {
-              "_soapheaders": {},
-              "files": {
-                "ListItems": {
-                  "DownloadFile": {
+        "_soapheaders": {},
+        "files": {
+            "ListItems": {
+                "DownloadFile": {
                     "FileRno": file_id
-                  }
                 }
-              },
-              "id": os.environ.get('proarc_user')
             }
+        },
+        "id": os.environ.get('proarc_user')
+    }
 
     # Continuing on the soap call
     if os.environ.get('transit_decode', 'false').lower() == "true":
-        rootlogger.info("transit_decode is set to True.")
+        LOG.info("transit_decode is set to True.")
         entity = typetransformer.transit_decode(entity)
 
-    rootlogger.info("Finished creating request: " + str(entity))
+    LOG.info(f"Finished creating request: {str(entity)}")
 
-    response = do_soap(entity, client, path)
-    rootlogger.info("SOAPResponse : \n" + str(response) + "\n----End-Response----")
+    response = do_soap(entity, SOAP_CLIENT, path)
+    LOG.info(f"SOAPResponse : \n{str(response)}\n----End-Response----")
     try:
-        filestream = upload_file(filename)
-    except Exception as e:
-        rootlogger.info("  Could not open " + filename + " :%s"% e)
-        return Response(response=("Could not open " + filename + " :%s"% e), status=404)
+        file_stream = read_file(filename)
+    except IOError as exc:
+        exc_message = f" Could not open {filename}: {exc}"
+        LOG.info(exc_message)
+        return Response(response=exc_message, status=404)
 
-    return Response(response=filestream, status=200)
+    return Response(response=file_stream, status=200)
 
 
 def download_file(url, local_filename):
-    r = requests.get(url, stream=True)
-    with open("/fileshare/"+local_filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
+    """
+    Function to download file from provided URL and store it on local file share
+    :param url: remote file location
+    :param local_filename: name of stored file
+    :return: local_filename
+    """
+    res = requests.get(url, stream=True)
+    with open("/fileshare/" + local_filename, 'wb') as file:
+        for chunk in res.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                file.write(chunk)
     return local_filename
 
 
-def upload_file(filename):
-    with open("/fileshare/"+filename, 'rb') as f:
-        return f.read()
+def read_file(filename):
+    """
+    Function to read file with given name  from file share
+    :param filename: name of file to be readed
+    :return: file content
+    """
+    with open("/fileshare/" + filename, 'rb') as file:
+        return file.read()
 
 
 def do_soap(entity, client, path):
-
+    """
+    Function to perform SOAP call
+    :param entity:
+    :param client:
+    :param path:
+    :return:
+    """
     headers = entity['_soapheaders']
-    filtered_entity = {i:entity[i] for i in entity if not i.startswith('_') }
-    filtered_entity['_soapheaders']=headers
+    filtered_entity = {i: entity[i] for i in entity if not i.startswith('_')}
+    filtered_entity['_soapheaders'] = headers
 
     response = getattr(client.service, path)(**filtered_entity)
     return response
 
 
 if __name__ == '__main__':
-    if rootlogger.isEnabledFor(logging.DEBUG):
-        app.run(debug=True, host='0.0.0.0', port=PORT)
+    if LOG.isEnabledFor(logging.DEBUG):
+        APP.run(debug=True, host='0.0.0.0', port=PORT)
     else:
         import cherrypy
 
-        cherrypy.tree.graft(app, '/')
+        cherrypy.tree.graft(APP, '/')
         cherrypy.config.update({
             'environment': 'production',
             'engine.autoreload_on': True,
